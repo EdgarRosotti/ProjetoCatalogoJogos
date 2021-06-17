56c8f058aeefd0ff0d90f6e0de87abe9a586d358
EdgarRosotti <edgarrosotti@hotmail.com>
2021-06-17 20:05:16 -0300

    Projeto Finalizado

---
+++ ProjetoCatalogoJogos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoCatalogoJogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
